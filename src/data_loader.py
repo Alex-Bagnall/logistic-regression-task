7159571a4da113542f6c183d855cff4877609a2e
alex bagnall <niscowal@gmail.com>
2026-02-04 19:43:12 +0000

    (Chore) Setup data_loader to take in data from csv and map the labels "Kecimen" & "Besni" to 0 and 1 respectively

---
+++ src/data_loader.py
@@ -0,0 +1,30 @@
+import csv
+import numpy as np
+
+class DataLoader:
+    def __init__(self, file_path:str):
+        self.file_path = file_path
+        pass
+
+    def load(self):
+        label_map = {"Kecimen": 0, "Besni": 1}
+
+        with open(self.file_path, 'r') as f:
+            reader = csv.reader(f)
+            headers = next(reader)
+
+            data = []
+            for row in reader:
+                features = [float(x) for x in row[:-1]]
+                label = label_map[row[-1]]
+                data.append(features + [label])
+
+        data = np.array(data)
+        features = data[:, :-1]
+        labels = data[:, -1]
+
+        return features, labels
+
+if __name__ == "__main__":
+    loader = DataLoader("../data/raisin_dataset.csv")
+    loader.load()
