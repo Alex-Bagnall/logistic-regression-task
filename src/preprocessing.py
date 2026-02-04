43ea1e5ecd96d2b0a683f1765be928946bdb2b4b
alex bagnall <niscowal@gmail.com>
2026-02-04 19:53:27 +0000

    (Chore) Add preprocessing with normalisation

---
+++ src/preprocessing.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from src.data_loader import DataLoader
+
+class Preprocessor:
+    def __init__(self):
+        self.std = None
+        self.mean = None
+
+    def create_normalised_array(self, features):
+        self.mean = np.mean(features, axis=0)
+        self.std = np.std(features, axis=0)
+        return (features - self.mean) / self.std
+
+    def preprocess(self):
+        data_loader = DataLoader("../data/raisin_dataset.csv")
+        features, labels = data_loader.load()
+        normalised_features = self.create_normalised_array(features)
+        indices = np.random.permutation(len(normalised_features))
+        split = int(len(normalised_features) * 0.8)
+
+        features_train = normalised_features[indices[:split]]
+        features_test = normalised_features[indices[split:]]
+        labels_train = labels[indices[:split]]
+        labels_test = labels[indices[split:]]
+        return features_train, features_test, labels_train, labels_test
+
+if __name__ == "__main__":
+    loader = Preprocessor()
+    loader.preprocess()
